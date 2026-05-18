9178f6dd726b5090c50f02dd1ee96082906e0752
Clay <claystonebarongo@gmail.com>
2026-05-18 21:15:41 +0300

    daily updates

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 import './Login.css'
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='container'>
+            <div className="header">
+                <div className="text">
+                    Sign Up
+                </div>
+                <div className="underline"></div>
+            </div>
+
+            <div className="inputs">
+
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="text" />
+                </div>
+
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="email" />
+                </div>
+
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="password" />
+                </div>
+
+            </div>
+        </div>
     )
 }
 

a46b3bed414e137087f0eaaf3320d3b898a9c9a3
Clay <claystonebarongo@gmail.com>
2026-05-26 21:39:13 +0300

    daily updates

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
 
 
 
-                    <input type="text" />
+                    <input type="text" />#
                 </div>
 
                 <div className="input">
